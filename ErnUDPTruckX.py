475729730ba402bf6c3968add02172f021ede5b3
maxwellington20 <57428636+maxwellington20@users.noreply.github.com>
2021-04-13 23:30:21 -0500

    Initial Commit
    The true innit

---
+++ ErnUDPTruckX.py
@@ -0,0 +1,108 @@
+# Program to simulate leading truck
+# by Maxwell Ern, 
+# The following program builds on the the following project by 
+# communicating with multiple servers. Largely unchanged. 
+
+import socket
+import time
+
+#initial client values, mostly taken from provided project files
+seq_num = 5895
+port_num = 6789
+pos = "14 S 368052 3899189"
+vel = 110.0
+t_sec = 0.2
+accel = 1.38
+brk_ctrl = 0.0
+throttle = 0.46
+
+
+#####################
+# Definitions Below #
+#####################
+
+#prepare client_pack data
+def createPacket():
+    client_pack = str(seq_num) + "," + str(addr) + "," + str(pos) + "," + str(vel)
+    client_pack += "," + str(accel) + "," + str(brk_ctrl) + "," + str(throttle) + ","
+    return str.encode(client_pack) #convert list of variables to bytes
+
+#print packet info
+def printPacketSent():
+    print("Packet Sent:")
+
+    if(seq_num == 99999): # signals that client will disconnect from server
+        print("Type = Socket Disconnection")
+        print("Sequence No. = 99999\n")
+        clientSock.close()
+        quit()
+
+    print("Sequence No. = %d" %(seq_num))
+    print("IP = %s" %(addr))
+    print("GPS Position = %s" %(pos))
+    print("Velocity = %.2f" %(vel))
+    print("Acceleration = %.2f" %(accel))
+    print("Brake Control = %.1f" %(brk_ctrl))
+    print("Gas Throttle = %.2f\n" %(throttle))
+
+#print ack info
+def printPacketRecv(num):
+    print("Packet Received:")
+    print("Type = Ack")
+    print("Sequence No. = %s\n" %(num))
+
+
+##################
+# The Rest Of It #
+##################
+
+#connect to server
+clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#get ip address
+addr=socket.gethostbyname(socket.gethostname())
+if addr=="127.0.0.1":
+    print("No internet, your localhost is "+ addr)
+else:
+    print("Connected, with the IP address: "+ addr)
+
+#print initial values
+print("\nInitial GPS Position: %s" %(pos))
+print("Initial Velocity: %.1fkm/h" %(vel))
+print("Time Interval: %.1fs" %(t_sec))
+print("============================================")
+
+def sendAndReceive(serv_addr): #made this to streamline the testing proccess
+    #send client packet
+    data = createPacket()
+    clientSock.sendto(data, (serv_addr, port_num))
+    printPacketSent()
+
+    while True:
+        time.sleep(t_sec) #wait a bit before looking to recieve
+        data, serv_addr = clientSock.recvfrom(1024) #recieve server packet
+        serv_pack = bytes.decode(data)
+        printPacketRecv(serv_pack)
+        break
+
+sendAndReceive(addr) #execute with original info
+
+
+# i am an undergrad and only doing the following tests to see if I can, so I
+# won't create ten different data exchanges like the grad requirement
+
+# in order to follow the scripts provided in the project1 folder on canvas,
+# I manually code changes in data here rather than in the terminal
+
+#new packet data
+seq_num += 1 
+pos = "14 S 368048 3899185"
+vel = 112.88
+accel = 2.63
+throttle = 0.46
+sendAndReceive(addr) #execute with new changes
+
+#final change
+seq_num = 99999 #program uses this sequence number to tell server it is disconnecting
+#sendAndReceive(addr)
+
---
+++ ErnUDPTruckZ.py
@@ -0,0 +1,166 @@
+# Program to simulate the second following truck
+# by Maxwell Ern, 
+# The following program builds on the previous project by allowing
+# connections with mulitple clients. 
+
+import socket
+import time
+
+#initial server values, mostly taken from provided project files
+port = 6789
+pos = "14 S 368058 3899192"
+vel = 110.0
+t_sec = 0.2
+
+
+####################################
+# Establishing Server Socket (UDP) #
+####################################
+
+#create socket
+servSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#get ip address
+addr=socket.gethostbyname(socket.gethostname())
+if addr=="127.0.0.1":
+    print("No internet, your localhost is "+ addr)
+else:
+    print("Connected, with the IP address: "+ addr)
+
+#bind to ip addr and port number
+servSock.bind((addr, port))
+
+
+#####################
+# Definitions Below #
+#####################
+
+#takes variable list and conjoins into a string
+def getVarString(x):
+    s1 = ""
+    return(s1.join(x))
+
+#prints a socket closing notification before ending program
+def socketClosed(num):
+    print("Type = Socket Disconnection")
+    print("Sequence No. = %s\n" %(num))
+    quit()
+
+#extracts variables from packet and prints results as they are interpreted
+def printPacketStats(packet):
+
+    print("Packet Received:")
+    arr = list(packet) #splits up packet variables into list
+    i = 0 #counter to go through arr list
+
+    # I originally wanted the following lines to be contained
+    # in one big while loop, but I kept getting IndexOutOfBounds 
+    # and ran out of time to solve the issue :(
+
+    #first is sequence number
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    seq_num = getVarString(temp)
+    i = i + 1
+
+    if(seq_num == "99999"): # my program uses sequence number to signal when
+        servSock.close()    # to close the socket
+        socketClosed(seq_num)
+    
+    print("Sequence No. = %s" %(seq_num))
+
+    #second is client ip address
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_ip = getVarString(temp)
+    i = i + 1
+    print("IP = %s" %(client_ip))
+
+    #third is client position
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_pos = getVarString(temp)
+    i = i + 1
+    print("GPS Position = %s" %(client_pos))
+    
+    #fourth is client velocity
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_velocity = getVarString(temp)
+    i = i + 1
+    print("Velocity = %s" %(client_velocity))
+
+    #fifth is client acceleration
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_accel = getVarString(temp)
+    i = i + 1
+    print("Acceleration = %s" %(client_accel))
+
+    #sixth is client brake control
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_bc = getVarString(temp)
+    i = i + 1
+    print("Brake Control = %s" %(client_bc))
+
+    #last one is client gas throttle
+    temp = []
+    while (arr[i] != ','):
+        temp.append(arr[i])
+        i = i + 1
+    client_gt = getVarString(temp)
+    i = i + 1
+    print("Gas Throttle = %s\n" %(client_gt))
+
+    return seq_num #returns packet's sequence number to use in the ack packet
+
+#prints the ack packet when successfully sent
+def printPacketSent(num):
+    print("Ack Packet Sent:")
+    print("Type = Ack")
+    print("Sequence No. = %s\n" %(num))
+
+
+##################
+# The Rest Of It #
+##################
+
+#print initial values
+print("\nInitial GPS Position: %s" %(pos))
+print("Initial Velocity: %.1fkm/h" %(vel))
+print("Time Interval: %.1fs" %(t_sec))
+print("============================================")
+
+#main while loop to operate under
+while True:
+    #receive packet from lead truck
+    data, addr = servSock.recvfrom(1024)
+    client_pack = bytes.decode(data)
+
+    #print received packet
+    ack_num = printPacketStats(client_pack)
+    
+    #create and send ack
+    data = str(ack_num)
+    ack = str.encode(data)
+    servSock.sendto(ack, addr)
+    printPacketSent(ack_num) #print the sent ack packet
+
+    time.sleep(t_sec) #wait a bit before receiving more packets
+
+
+
+
